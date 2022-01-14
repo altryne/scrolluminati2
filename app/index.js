f895d87ae4de8d2131dff7f2d0276b5fa477d3f1
Alex Volkov <alex.volkov@fundbox.com>
2022-01-14 01:15:07 -0700

    added wallet connect and moralis

--- app/index.js
+++ app/index.js
@@ -1,4 +1,12 @@
 import One from './demo-1'
+import Web3 from 'web3';
+import Moralis from 'moralis';
+
+
+const serverUrl = "https://oqwxgtkiflrz.usemoralis.com:2053/server";
+const appId = "nsRkwNGljvOddDMGUmg4x5g60DX94d54XxLrifkf";
+Moralis.start({ serverUrl, appId });
+
 
 const demo = document.body.getAttribute('data-id')
 
@@ -12,19 +20,72 @@ let thumb_url = 'https://images.raritysniffer.com/500/500/ipfs/'
 
 
 
-window.start = async function (){
+// add from here down
+async function login() {
+  let user = Moralis.User.current();
+  if (!user) {
+    user = await Moralis.authenticate();
+    connected(user);
+  }
+  
+}
 
-  //randomize 100 numbers between 0 and 8127
-  let randomNumbers = []
-  for(let i = 0; i < 12; i++){
-    randomNumbers.push(Math.floor(Math.random() * 8127))
+async function logOut() {
+  await Moralis.User.logOut();
+  console.log("logged out");
+  window.location.reload()
+}
+
+document.querySelector('#connect').onclick = login
+document.querySelector('#disconnect').onclick = logOut
+
+async function init(){
+  let user = Moralis.User.current();
+  if(user){
+    connected(user)
+  }else{
+    start() 
+  }}
+  
+init()
+
+async function connected(user){
+  document.querySelector('#connect_wallet').style.display = 'none'
+  document.querySelector('#wallet_address').style.display = 'block'
+  document.querySelector('#wallet').innerHTML = user.get("ethAddress")
+  let ens = await Moralis.Web3API.resolve.resolveAddress();
+  if(ens && ens.name){
+    
+    document.querySelector('#wallet').innerHTML = ens.name
+    document.querySelector('.demo-1__title').innerHTML = ens.name
   }
-  //for all random numbers, get the image from IPFS url and add to array 
-  let images = []
+  getNFTs(user)
+}
+
+async function getNFTs(user){
+  const userEthNFTs = await Moralis.Web3.getNFTs();
+  window.connectedNFTs = userEthNFTs.filter(nft => nft.token_address === '0x26badf693f2b103b021c670c852262b379bbbe8a')
+  console.log('found these pyramids', window.connectedNFTs)
+  start() 
+}
+
+async function start(){
   let requests = []
-  for(let i = 0; i < randomNumbers.length; i++){
-    requests.push(fetch(url + randomNumbers[i]))
+  let images = []
+  if(window.connectedNFTs && window.connectedNFTs.length){
+     requests = window.connectedNFTs.map(nft => {return fetch(nft.token_uri)})
+  }else{
+    let randomNumbers = [];
+    for(let i = 0; i < 12; i++){
+      randomNumbers.push(Math.floor(Math.random() * 8127))
+    }
+
+    //for all random numbers, get the image from IPFS url and add to array 
+    for(let i = 0; i < randomNumbers.length; i++){
+      requests.push(fetch(url + randomNumbers[i]))
+    }
   }
+
   try {
     let promises = await Promise.all(requests)
     for(let i = 0; i < promises.length; i++){
@@ -67,4 +128,3 @@ window.start = async function (){
   window.app = new One()
 }
 
-start() 
